9aff44cb38508c4ce5216e2d49b50038267d02c6
junhapark <5709psy@gmail.com>
2017-09-09 10:25:32 +0900

    junha

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -5,15 +5,21 @@
     .module('core')
     .controller('HeaderController', HeaderController);
 
-  HeaderController.$inject = ['$scope', '$state', 'Authentication', 'menuService'];
+  HeaderController.$inject = ['$scope', '$state', 'Authentication', 'menuService', '$http'];
 
-  function HeaderController($scope, $state, Authentication, menuService) {
+  function HeaderController($scope, $state, Authentication, menuService, $http) {
     var vm = this;
 
     vm.accountMenu = menuService.getMenu('account').items[0];
     vm.authentication = Authentication;
     vm.isCollapsed = false;
     vm.menu = menuService.getMenu('topbar');
+    vm.message = "늦으면 디짐";
+    vm.location = "광화문";
+
+    vm.openReservation = function () {
+      console.log('hi')
+    };
 
     $scope.$on('$stateChangeSuccess', stateChangeSuccess);
 
@@ -21,5 +27,43 @@
       // Collapsing the menu after navigation
       vm.isCollapsed = false;
     }
+
+
+    vm.background = function () {
+      if(vm.isCollapsed) {
+        // document.getElementById('content').style.opacity = 0.5;
+      }
+    };
+
+    var map = new naver.maps.Map('map');
+    var myaddress = '불정로 6';// 도로명 주소나 지번 주소만 가능 (건물명 불가!!!!)
+    naver.maps.Service.geocode({address: myaddress}, function(status, response) {
+      if (status !== naver.maps.Service.Status.OK) {
+        return alert(myaddress + '의 검색 결과가 없거나 기타 네트워크 에러');
+      }
+      var result = response.result;
+      // 검색 결과 갯수: result.total
+      // 첫번째 결과 결과 주소: result.items[0].address
+      // 첫번째 검색 결과 좌표: result.items[0].point.y, result.items[0].point.x
+      var myaddr = new naver.maps.Point(result.items[0].point.x, result.items[0].point.y);
+      map.setCenter(myaddr); // 검색된 좌표로 지도 이동
+      // 마커 표시
+      var marker = new naver.maps.Marker({
+        position: myaddr,
+        map: map
+      });
+      // 마커 클릭 이벤트 처리
+      naver.maps.Event.addListener(marker, "click", function(e) {
+        if (infowindow.getMap()) {
+          infowindow.close();
+        } else {
+          infowindow.open(map, marker);
+        }
+      });
+      // 마크 클릭시 인포윈도우 오픈
+      var infowindow = new naver.maps.InfoWindow({
+        content: '<h4> [네이버 개발자센터]</h4><a href="https://developers.naver.com" target="_blank"><img src="https://developers.naver.com/inc/devcenter/images/nd_img.png"></a>'
+      });
+    });
   }
 }());
--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -7,5 +7,112 @@
 
   function HomeController() {
     var vm = this;
+
+    vm.invite = function () {
+
+    };
+
+    vm.friends = [
+      {
+        name: "박준하",
+        phone: "010-3030-2811",
+        invited: false
+      },
+      {
+        name: "김지섭",
+        phone: "010-4030-2811",
+        invited: false
+      },
+      {
+        name: "유오성",
+        phone: "010-9030-2811",
+        invited: false
+      },
+      {
+        name: "최필준",
+        phone: "010-5530-2811",
+        invited: false
+      },
+      {
+        name: "박영선",
+        phone: "010-8830-2811",
+        invited: false
+      },
+      {
+        name: "박준하",
+        phone: "010-3030-2811",
+        invited: false
+      },
+      {
+        name: "김지섭",
+        phone: "010-4030-2811",
+        invited: false
+      },
+      {
+        name: "유오성",
+        phone: "010-9030-2811",
+        invited: false
+      },
+      {
+        name: "최필준",
+        phone: "010-5530-2811",
+        invited: false
+      },
+      {
+        name: "박영선",
+        phone: "010-8830-2811",
+        invited: false
+      },
+      {
+        name: "박준하",
+        phone: "010-3030-2811",
+        invited: false
+      },
+      {
+        name: "김지섭",
+        phone: "010-4030-2811",
+        invited: false
+      },
+      {
+        name: "유오성",
+        phone: "010-9030-2811",
+        invited: false
+      },
+      {
+        name: "최필준",
+        phone: "010-5530-2811",
+        invited: false
+      },
+      {
+        name: "박영선",
+        phone: "010-8830-2811",
+        invited: false
+      },
+      {
+        name: "박준하",
+        phone: "010-3030-2811",
+        invited: false
+      },
+      {
+        name: "김지섭",
+        phone: "010-4030-2811",
+        invited: false
+      },
+      {
+        name: "유오성",
+        phone: "010-9030-2811",
+        invited: false
+      },
+      {
+        name: "최필준",
+        phone: "010-5530-2811",
+        invited: false
+      },
+      {
+        name: "박영선",
+        phone: "010-8830-2811",
+        invited: false
+      }
+    ]
   }
 }());
